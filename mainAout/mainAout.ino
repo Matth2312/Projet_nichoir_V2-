b4c7fc53eb6246fd4a6f334959fc8954ab585f2c
Matth2312 <matthias.roberty@gmail.com>
2026-09-03 13:55:17 +0200

    Commentaires
    Modifications légères et commentaires pour présentation

--- mainAout/mainAout.ino
+++ mainAout/mainAout.ino
@@ -6,34 +6,34 @@
 #include <PubSubClient.h>
 #include <WebServer.h>
 #include <DNSServer.h>
-#include <Preferences.h>
+#include <Preferences.h>  //pour stocker de données dans la mémoire flash qui survive au deepsleep, >< mémoire RAM
 #include "driver/rtc_io.h"
 #include "driver/gpio.h"
 #include "esp_sleep.h"
 #include "esp_bt.h"          // btStop()
-//upload git02_09
+//upload git03_09
 
 // ================= CONFIGURATION =================
 
 // MQTT
 const char* MQTT_SERVER = "192.168.2.4";
 const uint16_t MQTT_PORT = 1883;
-const char* TOPIC_IMAGE = "nichoir/image";
-const char* TOPIC_BAT   = "nichoir/batterie";
+const char* TOPIC_IMAGE = "nichoir/image"; //topic d'abonnement publish/subscribe
+const char* TOPIC_BAT   = "nichoir/batterie";  //topic d'abonnement publish/subscribe
 
 // Point d'Acces (config WiFi initiale)
 const char* AP_SSID     = "NichoirConfigTHERER";
 const char* AP_PASSWORD = "12345678";
 
 #define PIR_WAKEUP_PIN GPIO_NUM_4
-#define HEARTBEAT_SEC (24UL * 60UL * 60UL) 
-#define BM8563_COOLDOWN_SEC (6UL * 60UL)     // 6 minutes
+#define HEARTBEAT_SEC (24UL * 60UL * 60UL) //=24h
+#define BM8563_COOLDOWN_SEC (5UL * 60UL)     //cool down de 5 min après détection
 #define POWER_HOLD_PIN GPIO_NUM_33
 
 // ================= OBJETS GLOBAUX =================
 
 WiFiClient espClient;
-PubSubClient mqttClient(espClient);
+PubSubClient mqttClient(espClient); //on utilise la connexion mqttClient sur wifi
 MyTimerCam Camera;
 Preferences preferences;
 WebServer server(80);
@@ -136,9 +136,9 @@ void startConfigAP() {
 // ================= WIFI (station) =================
 
 bool connectWiFiSTA() {
-  if (wifiSsid == "") return false;
+  if (wifiSsid == "") return false; //force le Ssid non nul
 
-  Serial.printf("[WIFI] Connexion a '%s'...\n", wifiSsid.c_str());
+  Serial.printf("[WIFI] Connexion a '%s'...\n", wifiSsid.c_str()); 
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifiSsid.c_str(), wifiPass.c_str());
 
@@ -161,6 +161,7 @@ bool connectWiFiSTA() {
 
 bool connectMQTT() {
   mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+
   if (!mqttClient.connected()) {
     Serial.print("[MQTT] Connexion...");
     String clientId = "TimerCAM-" + String((uint32_t)ESP.getEfuseMac(), HEX);
@@ -173,32 +174,32 @@ bool connectMQTT() {
   return mqttClient.connected();
 }
 
-void sendBattery() {
-  int voltage_mV = TimerCAM.Power.getBatteryVoltage();
-  int level_pct  = TimerCAM.Power.getBatteryLevel();
+void sendBattery() {                                              //modifié sur la focntion de juin, utilisation de la batterie //READ AND SEND battery
+  int voltage_mV = TimerCAM.Power.getBatteryVoltage();            //getBattery = méthode //Power est un objet
+  int niveauBAT  = TimerCAM.Power.getBatteryLevel();              //de la librairie TimerCam, renvoie l'info en pourcentage (considère 5V à 100%)
 
-  String msg = String(voltage_mV / 1000.0, 2); // en Volts, comme avant
-  Serial.printf("[BAT] %d mV (%d %%)\n", voltage_mV, level_pct);
-  mqttClient.publish(TOPIC_BAT, msg.c_str());
+  String msg = String(voltage_mV / 1000.0, 2);                    // en Volts, comme avant //2 pour deux décimales
+  Serial.printf("[BAT] %d mV (%d %%)\n", voltage_mV, niveauBAT);
+  mqttClient.publish(TOPIC_BAT, msg.c_str());  //msg.c_str = convertion en string
 }
 
 void sendImage(camera_fb_t* fb) {
-  if (!fb || fb->len == 0) return;
+  if (!fb || fb->len == 0) return; //len = taille de l'image en octets
 
   if (mqttClient.getBufferSize() < fb->len + 500) {
     mqttClient.setBufferSize(fb->len + 500);
   }
 
   Serial.printf("[MQTT] Envoi Image (%u bytes)... ", (unsigned)fb->len);
-  bool ok = mqttClient.publish(TOPIC_IMAGE, (const uint8_t*)fb->buf, fb->len, false);
+  bool ok = mqttClient.publish(TOPIC_IMAGE, (const uint8_t*)fb->buf, fb->len, false);  //buf =  adresse mémoire ou on a les octets de l'image jpep
   Serial.println(ok ? "OK" : "ECHEC");
 }
 
 // ================= LES DEUX EVENEMENTS POSSIBLES EN SENTINELLE =================
 
-// Reveil PIR (EXT0) : photo + batterie, puis cooldown BM8563 (6 min)
+// Reveil PIR (EXT0) : photo + batterie, puis cooldown BM8563 
 void handlePIRDetection() {
-  Serial.println("\n=== [EVENT] Mouvement detecte sur le PIR ===");
+  //Serial.println("\n=== [EVENT] Mouvement detecte sur le PIR ===");
   TimerCAM.Power.setLed(255);
 
   if (connectWiFiSTA()) {
@@ -268,7 +269,7 @@ void goToSentinelSleep() {
 }
 
 void goToBM8563Cooldown() {
-  Serial.println("[SLEEP] Cooldown BM8563 (6 min) apres detection");
+  Serial.println("Cooldown via BM8563 de 5min apres detection");
 
   
   preferences.begin("nichoir", false);
@@ -283,44 +284,43 @@ void goToBM8563Cooldown() {
   TimerCAM.Power.powerOff();
 }
 
-// ================= SETUP =================
+// ======================================================================== SETUP ===============================================================================
 
 void setup() {
-  Serial.begin(115200);
-  delay(200);
+  // Serial.begin(115200);
+  // delay(200);
 
-  gpio_hold_dis(POWER_HOLD_PIN);
-  gpio_deep_sleep_hold_dis();
+  gpio_hold_dis(POWER_HOLD_PIN);   //disable teh GPIO33 for the batterie
+  gpio_deep_sleep_hold_dis();   //disable all gpio put on hold after any sleep
 
   TimerCAM.begin(true);     // I2C pour le BM8563 (RTC)
   TimerCAM.Power.begin();   // maintien alim (GPIO33) + ADC batterie + LED (GPIO2)
 
-  // --- Cas 1 : on revient d'un cooldown BM8563 (apres une detection) ---
-  preferences.begin("nichoir", false);
-  bool returningFromCooldown = preferences.getBool("bm8563_ret", false);
+  //Cas 1 : on revient d'un cooldown BM8563 (apres une detection)
+  preferences.begin("nichoir", false);  //on créer en namespace (espace en mémoire) nomé nichoir
+  bool returningFromCooldown = preferences.getBool("bm8563_ret", false);  //flag de mise en sommeil profond  //le false correspond à une lecture de la variable
   if (returningFromCooldown) {
     preferences.putBool("bm8563_ret", false);
     preferences.end();
-    Serial.println("[BOOT] Retour du cooldown BM8563 -> reprise en sentinelle");
-    goToSentinelSleep();
+    //Serial.println("[BOOT] Retour du cooldown BM8563 -> reprise en sentinelle");
+    goToSentinelSleep();  //go back in deepsleep native because no need to reconnect wifi wihtout any detection of the pir 
     return;
   }
   preferences.end();
 
-  // --- Chargement des identifiants WiFi ---
-  preferences.begin("wifi", true);
+  // -------------------------------------------------------- Chargement des identifiants WiFi ----------------------
+  preferences.begin("wifi", true);  //reading only of the namespace wifi
   wifiSsid = preferences.getString("ssid", "");
   wifiPass = preferences.getString("password", "");
   preferences.end();
-
   if (wifiSsid == "") {
   
     Serial.println("[WIFI] Pas de SSID sauvegarde -> Mode AP");
     isStationMode = false;
-    startConfigAP();
+    startConfigAP();  //allow the esp32 to become a wifi accès point to connect my phone and give the password (already present in the main of june)
     return;
   }
-  isStationMode = true;
+  isStationMode = true; //the esp32 has already got the indentities
 
   // --- Cas 2 : identification de la cause du reveil (mode sentinelle) ---
   esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
@@ -328,12 +328,12 @@ void setup() {
   if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT0) {
     handlePIRDetection();
     goToBM8563Cooldown();
-    return; // jamais atteint
+    return;
   }
   else if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER) {
-    handleHeartbeat();
+    handleHeartbeat();  //sent battery level
     goToSentinelSleep();
-    return; // jamais atteint
+    return;
   }
   else {
     Serial.println("[BOOT] Premier demarrage");
